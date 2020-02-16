9ace2dcc17c3c46b9bdd64a6f233f75d27feb1df
Purwo Widodo <id.purwowd@gmail.com>
2020-02-16 23:49:25 +0700

    Updated admin to support custom user model.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,7 +6,7 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         email = "id.purwowd@gmail.com"
-        password = 'Testpass123'
+        password = 'test123'
         user = get_user_model().objects.create_user(
             email=email,
             password=password
